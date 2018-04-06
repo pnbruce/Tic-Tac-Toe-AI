600f7ce4eb228b7850734e039b4c7c313678075a
Patrick B <patricknbruce@gmail.com>
2018-04-05 19:33:54 -0500

    Initial Commit

---
+++ Tic Tac Toe AI/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Tic Tac Toe AI
+//
+//  Created by Patrick Bruce on 4/5/18.
+//  Copyright © 2018 Patrick Bruce. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
